0100aa943abaa6b4fde4dbc2b69ae31f75d5c4c9
Alejandro <Alejandro.Nieto3@alu.uclm.es>
2021-05-21 17:44:03 +0200

    Errorcillo del payment

--- carreful/src/main/webapp/js/viewModels/payment.js
+++ carreful/src/main/webapp/js/viewModels/payment.js
@@ -9,7 +9,8 @@ define([ 'knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils',
 			self.stripe = Stripe('pk_test_51IdbtOE3xk4z0l3iOwpaJ3Rp0n58pBWBVBVxrba7Vslzdk28K2SCTtqYgk16LXkXthMQ5kZQQPaTkMr34BLL6BlJ00AKbD4VQZ');
 			self.carrito = ko.observableArray([]);
 			self.total = ko.observable(0);
-			self.envio = ko.observable("");
+			self.envio = ko.observable("Express");
+			
 			
 			self.message = ko.observable();
 			self.error = ko.observable();
@@ -164,10 +165,8 @@ define([ 'knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils',
 		
 		Probando(){
 			let self = this;
-			let prueba = envio;
-
 			let data = {
-				url : "payments/confirmarPedido/"+ envio,
+				url : "payments/confirmarPedido/"+ self.envio(),
 				type : "get",
 				contentType : 'application/json',
 				success : function(response) {
