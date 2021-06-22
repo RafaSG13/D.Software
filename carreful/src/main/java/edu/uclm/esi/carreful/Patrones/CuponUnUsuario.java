cfda9b6173054b82c06f2722877d2cd066f0c89a
Sergio <Sergio.perez18@alu.uclm.es>
2021-06-22 11:14:39 +0200

    Añadir cupones un usuario

--- carreful/src/main/java/edu/uclm/esi/carreful/Patrones/CuponUnUsuario.java
+++ carreful/src/main/java/edu/uclm/esi/carreful/Patrones/CuponUnUsuario.java
@@ -1,4 +1,6 @@
 package edu.uclm.esi.carreful.Patrones;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,10 +13,11 @@ import edu.uclm.esi.carreful.model.Cupon;
 
 @Entity
 public class CuponUnUsuario extends Cupon{
-	
+	private String usuario;
 
 	public CuponUnUsuario(String codigo, Date fechaInicio, Date fechaFin, double descuento, String tipoDescuento, String usuario) {
 		super(fechaInicio,fechaFin,descuento,tipoDescuento);
+		this.usuario= usuario;
 
 	}
 	
@@ -22,6 +25,14 @@ public class CuponUnUsuario extends Cupon{
 		super();
 	}
 	
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
 	@Override
 	public void usarCupon() {
 		System.out.println("Estoy siendo usado, Cupon Un uso por Usuario");
@@ -30,8 +41,15 @@ public class CuponUnUsuario extends Cupon{
 
 	@Override
 	public void usarCupon(String email) {
+		usuario= usuario+ ", "+ email;
 		System.out.println("Estoy siendo usado, Cupon Un uso por Usuario");
 		
+	}
+
+	public boolean contieneUsuario(String email) {
+		ArrayList<String> usuarios = new ArrayList<String>(Arrays.asList(usuario.split(", ")));
+		if (usuarios.contains(email)) return true;
+		return false;
 	}
 
 
--- carreful/src/main/java/edu/uclm/esi/carreful/http/PaymentsController.java
+++ carreful/src/main/java/edu/uclm/esi/carreful/http/PaymentsController.java
@@ -48,172 +48,161 @@ public class PaymentsController extends CookiesController {
 	static {
 		Stripe.apiKey = "sk_test_51IdbtOE3xk4z0l3iN9AVWJ8eQSx7Ifhhk13OAeKS3TQjW2eN66yCmS3xwXV265bvot1p0ldgabUugmSLk3310dP000A4Z1Kszx";
 	}
-	
+
 	@Autowired
 	CuponUnUsoDao cuponUnUsoDao;
-	
+
 	@Autowired
 	CuponUnUsuarioDao cuponUnUsuarioDao;
-	
+
 	@Autowired
 	CuponMultipleDao cuponMultipleDao;
-	
+
 	@Autowired
 	CorderDao corderDao;
-		
+
 	@Autowired
 	UserDao userDao;
-	
+
 	@PostMapping("/solicitarPreautorizacion")
 	public String solicitarPreautorizacion(HttpServletRequest request, @RequestBody Map<String, Object> info) {
 		try {
-			Carrito carrito=(Carrito) request.getSession().getAttribute("carrito");
-			if(carrito==null) {
+			Carrito carrito = (Carrito) request.getSession().getAttribute("carrito");
+			if (carrito == null) {
 				carrito = new Carrito();
-				request.getSession().setAttribute("carrito",carrito);
+				request.getSession().setAttribute("carrito", carrito);
 			}
-			if(carrito.getProducts().isEmpty()) throw new CarrefulException(HttpStatus.FORBIDDEN,"No hay productos en el Carrito, por favor, "
-					+ "incluya productos en el carrito para poder pagar");
-			
-			PaymentIntentCreateParams createParams = new PaymentIntentCreateParams.Builder()
-					.setCurrency("eur")
-					.setAmount((long) precioTotal(request)*100)
-					.build();
+			if (carrito.getProducts().isEmpty())
+				throw new CarrefulException(HttpStatus.FORBIDDEN, "No hay productos en el Carrito, por favor, "
+						+ "incluya productos en el carrito para poder pagar");
+
+			PaymentIntentCreateParams createParams = new PaymentIntentCreateParams.Builder().setCurrency("eur")
+					.setAmount((long) precioTotal(request) * 100).build();
 			// Create a PaymentIntent with the order amount and currency
 			PaymentIntent intent = PaymentIntent.create(createParams);
 			JSONObject jso = new JSONObject(intent.toJson());
-			
-			
-			
+
 			return jso.getString("client_secret");
-		}catch (StripeException e) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Lo sentimos, pero el  pedido minimo a cobrar debe superar los 0,50 euros.");
+		} catch (StripeException e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+					"Lo sentimos, pero el  pedido minimo a cobrar debe superar los 0,50 euros.");
 		} catch (CarrefulException e) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,e.getMessage());
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
 		}
 	}
-	
-	@GetMapping("/confirmarPedido/{correo}")
-	public String confirmarPedido(HttpServletRequest request, @PathVariable String correo) {
+
+	@GetMapping("/confirmarPedido")
+	public String confirmarPedido(HttpServletRequest request) {
 		try {
-			Carrito carrito=(Carrito) request.getSession().getAttribute("carrito");
-			Cupon cupon = carrito.getCuponDescuento();
+			Carrito carrito = (Carrito) request.getSession().getAttribute("carrito");
 			User user = userDao.findByEmail((String) request.getSession().getAttribute("userEmail"));
 
-			if(user!=null) correo = user.getEmail(); 
-			cupon.usarCupon(correo);
-			//CuponDao.save(cupon);
-			//CuponDao.delete(cupon);
-			
 			Corder pedido = new Corder();
 			pedido.setPrecioTotal(precioTotal(request));
 			pedido.setState("Preparandose");
 			pedido.setPedido(sacarProductos(carrito.getProducts().iterator()));
-			
+
 			corderDao.save(pedido);
-			
-			if (user!=null) {
+
+			if (user != null) {
 				Token token = new Token((String) request.getSession().getAttribute("userEmail"));
 				tokenDao.save(token);
 				Email smtp = new Email();
-				String texto = "Su pedido es el siguiente: " + 
-						"<a href='http://localhost/user/usarToken/" + token.getId() + "'>aquí</a>";
+				String texto = "Su pedido es el siguiente: " + "<a href='http://localhost/user/usarToken/"
+						+ token.getId() + "'>aquí</a>";
 				smtp.send(user.getEmail(), "Carreful confirmacion de Pedido.", texto);
 			}
-			
-			
-			return "Compra realizada con exito\nPedido con numero: "+pedido.getId();
-			
+
+			return "Compra realizada con exito\nPedido con numero: " + pedido.getId();
+
 		} catch (Exception e) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
 		}
 	}
-	
+
 	@PostMapping("/AplicarDescuento/{cupon}")
 	public void AplicarDescuento(HttpServletRequest request, @PathVariable String cupon) {
 		try {
 			Carrito carrito = (Carrito) request.getSession().getAttribute("carrito");
-			if(carrito==null) { //Si no hay carrito en la session lo crea y lo inserta.
-				carrito =  new Carrito();
-				request.getSession().setAttribute("carrito",carrito);
-			}
-			
-			Optional<CuponUnUso> optcuponUnUso = cuponUnUsoDao.findById(cupon);
-			Optional<CuponMultiple>optcuponMultiple = cuponMultipleDao.findById(cupon);
-			Optional<CuponUnUsuario>optcuponUnUsuario = cuponUnUsuarioDao.findById(cupon);
-					
-	
-			if(!optcuponUnUso.isPresent() && !optcuponMultiple.isPresent())
-				throw new CarrefulException(HttpStatus.NOT_FOUND,"El cupon introducido no existe");
-			
-			if(optcuponUnUso.isPresent() && !optcuponUnUso.get().isUsado())
-				carrito=introducirCuponEnCarrito(request,optcuponUnUso.get());
-				
-				
-			
-			if(optcuponUnUsuario.isPresent()) {
-				
-				carrito = introducirCuponEnCarrito(request,optcuponUnUsuario.get());
+			String email = (String) request.getSession().getAttribute("email");
+			if (carrito == null) { // Si no hay carrito en la session lo crea y lo inserta.
+				carrito = new Carrito();
+				request.getSession().setAttribute("carrito", carrito);
 			}
 
-			if(optcuponMultiple.isPresent())
-				carrito = introducirCuponEnCarrito(request,optcuponMultiple.get());
-			
+			Optional<CuponUnUso> optcuponUnUso = cuponUnUsoDao.findById(cupon);
+			Optional<CuponMultiple> optcuponMultiple = cuponMultipleDao.findById(cupon);
+			Optional<CuponUnUsuario> optcuponUnUsuario = cuponUnUsuarioDao.findById(cupon);
+
+			if (!optcuponUnUso.isPresent() && !optcuponMultiple.isPresent() && !optcuponUnUsuario.isPresent())
+				throw new CarrefulException(HttpStatus.NOT_FOUND, "El cupon introducido no existe");
+
+			if (optcuponUnUso.isPresent() && !optcuponUnUso.get().isUsado())
+				carrito = introducirCuponEnCarrito(request, optcuponUnUso.get());
+
+			if (optcuponUnUsuario.isPresent() && !optcuponUnUsuario.get().contieneUsuario(email)) {
+
+				carrito = introducirCuponEnCarrito(request, optcuponUnUsuario.get());
+			}
+
+			if (optcuponMultiple.isPresent())
+				carrito = introducirCuponEnCarrito(request, optcuponMultiple.get());
+
 			request.getSession().setAttribute("carrito", carrito);
-			
-			}catch(CarrefulException e) {
+
+		} catch (CarrefulException e) {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
 		}
-	
-		
+
 	}
-	
+
 	@GetMapping("/PrecioTotal")
 	public double precioTotal(HttpServletRequest request) {
-		double total=0;
+		double total = 0;
 		Carrito carrito = (Carrito) request.getSession().getAttribute("carrito");
-		if (carrito==null) {
+		if (carrito == null) {
 			carrito = new Carrito();
 			request.getSession().setAttribute("carrito", carrito);
 		}
-			
-		Iterator<OrderedProduct> iterador_productos=carrito.getProducts().iterator();
-		while(iterador_productos.hasNext()) {
+
+		Iterator<OrderedProduct> iterador_productos = carrito.getProducts().iterator();
+		while (iterador_productos.hasNext()) {
 			OrderedProduct aux = iterador_productos.next();
-			total+=aux.getAmount()*aux.getPrecio();
+			total += aux.getAmount() * aux.getPrecio();
 		}
-		if(carrito.getCuponDescuento()!=null) {
-			if(carrito.getCuponDescuento().getTipoDescuento().equalsIgnoreCase("porcentual")) {
-				total = total - (total*carrito.getCuponDescuento().getDescuento());
+		if (carrito.getCuponDescuento() != null) {
+			if (carrito.getCuponDescuento().getTipoDescuento().equalsIgnoreCase("porcentual")) {
+				total = total - (total * carrito.getCuponDescuento().getDescuento() / 100);
 			}
-				
-			else if(carrito.getCuponDescuento().getTipoDescuento().equalsIgnoreCase("fijo"))
+
+			else if (carrito.getCuponDescuento().getTipoDescuento().equalsIgnoreCase("fijo"))
 				total = total - carrito.getCuponDescuento().getDescuento();
 		}
-		if(total<0) // si el descuento hace que el precio sea negativo, entonces lo cambiamos a que sea como minimo GRATIS
+		if (total < 0) // si el descuento hace que el precio sea negativo, entonces lo cambiamos a que
+						// sea como minimo GRATIS
 			total = 0;
 		return total;
 
 	}
-		
-	public String sacarProductos(Iterator<OrderedProduct> productos){
+
+	public String sacarProductos(Iterator<OrderedProduct> productos) {
 		String pedido = "";
-		while(productos.hasNext()) {
+		while (productos.hasNext()) {
 			OrderedProduct aux = productos.next();
-			pedido+=aux.getName()+","+aux.getAmount()+","+aux.getPrecio()+"\n";
+			pedido += aux.getName() + "," + aux.getAmount() + "," + aux.getPrecio() + "\n";
 		}
-		return pedido;	
+		return pedido;
 	}
-	
-	private Carrito introducirCuponEnCarrito(HttpServletRequest request,Cupon cupon) throws CarrefulException {
+
+	private Carrito introducirCuponEnCarrito(HttpServletRequest request, Cupon cupon) throws CarrefulException {
 		Carrito carrito = (Carrito) request.getSession().getAttribute("carrito");
-		
-		if(cupon.getRango().comprobarValidez(Calendar.getInstance().getTime())) {
+
+		if (cupon.getRango().comprobarValidez(Calendar.getInstance().getTime())) {
 			carrito.setCuponDescuento(cupon);
 			request.getSession().setAttribute("carrito", carrito);
-			
-		}
-		else throw new CarrefulException(HttpStatus.FORBIDDEN,"El cupon no es valido a dia de hoy");
+
+		} else
+			throw new CarrefulException(HttpStatus.FORBIDDEN, "El cupon no es valido a dia de hoy");
 		return carrito;
 	}
 }
